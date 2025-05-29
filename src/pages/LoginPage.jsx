37a490a1321ce73b39e4131ced032b4aa190186a
sarthakyeole <yeolesarthakdev@gmail.com>
2025-05-30 00:22:16 +0530

    frontend for now is over

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -21,7 +21,7 @@ const LoginPage = () => {
   return (
     <div className="min-h-screen bg-cover bg-center flex items-center justify-center gap-8 sm:justify-evenly max-sm:flex-col backdrop-blur-2xl">
       {/* ------------------left------------------- */}
-      <img src={assets.logo_big} alt="" className="w-[min(30vw, 250px)]" />
+      <img src={assets.logo_big} alt="" className="w-[min(25vw,175px)]" />
 
       {/* ----------------right------------------ */}
 
